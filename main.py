0f335aeed9f2dddc52e42aa830b2d54f05d8cb6f
CrashLogger <erlantz.alonso@live.com>
2026-02-21 00:05:58 +0100

    Auto update

--- main.py
+++ main.py
@@ -17,16 +17,22 @@ intents.message_content = True
 # Loggeamos el bot con el token del .env
 bot =  discord.Client(intents=intents)
 
+# Cargamos las autoresponses posibles de este archivo
 autoresponse_path = "autoresponse"
 working_dir = os.getcwd()
 
 io_helper = iohelpers.ioHelpers(autoresponse_path)
 io_helper.load_responses()
-# Cargamos las autoresponses posibles
-
-# Eventos a los que el bot responde
+###################################################
+# Eventos a los que el bot responde               #
+###################################################
+# Cosas que hace al arranque
 @bot.event
-# Mensaje con string específica
+async def on_ready():
+    bot.loop.create_task(io_helper.start_periodic_reload(interval=30))
+
+# Cosas que hace al buscar mensajes
+@bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
@@ -34,11 +40,9 @@ async def on_message(message):
         print("Woah mr hacker over here")
         print(io_helper.file_name_list)
         print(io_helper.file_types_list)
-        await message.channel.send("My replies are:")
-        await message.channel.send(io_helper.file_name_list)
-        await message.channel.send("And their formats are:")
-        await message.channel.send(io_helper.file_types_list)
-        
+        await message.channel.send(f"My replies are:```{io_helper.file_name_list}```And their formats are:\n```{io_helper.file_types_list}```")
+
+
     elif message.content in io_helper.file_name_list:
         file_path = os.path.join(working_dir, autoresponse_path, io_helper.file_list[io_helper.file_name_list.index(message.content)])
         if io_helper.file_types_list[io_helper.file_name_list.index(message.content)] in io_helper.TEXT_FORMATS:
--- model/iohelpers.py
+++ model/iohelpers.py
@@ -1,4 +1,5 @@
 import os
+import asyncio
 
 class ioHelpers:
 
@@ -9,15 +10,33 @@ class ioHelpers:
         self.file_name_list = []
         self.TEXT_FORMATS = [".txt", ".md"]
 
-    def load_responses(self):
-        self.file_list = os.listdir(self.autoresponse_path)
-        for file in self.file_list:
-            file, filetype = os.path.splitext(file)
-            self.file_name_list.append(file)
-            self.file_types_list.append(filetype)
-        print(self.file_name_list)
+    async def load_responses(self):
 
+        # Si alguien tiene una forma mas elegante de hacer esto que me mande PR
+        # Lo he hecho para minimizar el tiempo de cambio de listas
+        
+        tmp_file_list = []
+        tmp_file_types_list = []
+        tmp_file_name_list = []
+
+        tmp_file_list = os.listdir(self.autoresponse_path)
+        for file in tmp_file_list:
+            file, filetype = os.path.splitext(file)
+            tmp_file_name_list.append(file)
+            tmp_file_types_list.append(filetype)
+
+        self.file_list = tmp_file_list
+        self.file_types_list = tmp_file_types_list
+        self.file_name_list = tmp_file_name_list
+
+    # Si queréis podemos hacer que mande multiples mensajes de mas de 2kB
+    # No sé por que querríais que hiciera eso, pero en fin.
     def read_text_file(self, path:str):
         with open(path, 'r', encoding='utf-8') as f:
             text = f.read(2000)
             return(text)
+        
+    async def start_periodic_reload(self, interval: int = 60):
+        while True:
+            await self.load_responses()
+            await asyncio.sleep(interval)
