032955645c76787af0cfe4b33cb1740ddea2c128
pmartine082 <pmartinez082@ikasle.ehu.eus>
2026-02-21 17:19:11 +0100

    holi otra vez

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from controller.bot import run_bot
 
 if __name__ == '__main__':
    
-    run_api()
+   
 
     # Start Flask API in a separate thread
-    '''
+    
     api_thread = Thread(target=run_api)
     api_thread.daemon = True
     api_thread.start()
 
     # Start Discord bot in the main thread
     run_bot()
-    '''
+    
